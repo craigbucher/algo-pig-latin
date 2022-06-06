d0a07e19cba6d74624bfdf3477b37149dea7fc87
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-06 14:20:49 -0500

    js base case

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,5 +1,16 @@
 exports.translate = function(english) {
     let splitWords = english.split(' ');
-    let pigLatin = '';
-    let splitPigLatin = [];
+    let pigLatin = [];
+    for (let word of splitWords) {
+        // check to see if begins with letter
+        if ((/(^[aeiouAEIOU])/).test(word)){
+            pigLatin.push(word[0] + word.substring(1) + "ay");
+        } else if ((/(^[^aeiouAEIOU])/).test(word)){
+            pigLatin.push(word.substring(1) + word[0] + "ay");
+        } else {
+            pigLatin.push(word);
+        }
+    }
+    console.log(pigLatin)
 };
+console.log(exports.translate("banana"));
