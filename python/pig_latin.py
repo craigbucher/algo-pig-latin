884cd939305c4fba98deb69d8a82ee4b606366c1
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-06 16:28:57 -0500

    added comments

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -4,21 +4,28 @@ def translate(english):
   split_words = english.split(' ');
   pig_latin = []
   for word in split_words:
+    # word starts with 'sch' as in school
     if re.match('^sch|Sch', word):
       pig_latin.append(word[3:] + word[0:3] + "ay")
+    # qu is second consonant sound
     elif re.match('^([^aeiouAEIOU][qQ]u)', word):
       pig_latin.append(word[3:] + word[0:3] + "ay")
+    #word starts with qu
     elif re.match('^(qu|Qu)', word):
       pig_latin.append(word[2:] + word[0:2] + "ay")
+    # starts with 3 consonants
     elif re.match('^([^aeiouAEIOU]){3}', word):
       pig_latin.append(word[3:] + word[0:3] + "ay")
+    # starts with 2 consonants
     elif re.match('^([^aeiouAEIOU]){2}', word):
       pig_latin.append(word[2:] + word[0:2] + "ay")
+    # if starts with a vowel, just add 'ay' to the end
     elif re.match('(^[aeiouAEIOU])', word):
       pig_latin.append(word[0] + word[1:] + "ay")
+    #if starts with a consonant, add first letter and 'ay' to the end
     elif re.match('^([^aeiouAEIOU])', word):
       pig_latin.append(word[1:] + word[0] + "ay")
-
+      
   return ' '.join(pig_latin)
 
-#print(translate('the quick brown fox'))
+#print(translate('This is a test'))
